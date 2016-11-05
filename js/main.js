4c6be04f3382f807f7b2c688b604208fe921af5b
Alberto Hernández <quienquieraquesea_123@hotmail.com>
2016-11-04 23:48:43 -0600

    NPI

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
-var hola = "hola";
-var 
+$('#enviar').click(function(e) {
+
+	var persona = $('#usuario');
+
+	var respuesta = persona.val();
+
+	 alert(respuesta);
+
+});
+
+$(respuesta).appendTo( '#enviar' );
